2ef75b22dd75aa69fb4a2461472734f3acc837f1
Mico Ellerich Comia <micocomia@gmail.com>
2021-04-06 14:02:17 +0800

    Added activity 1.1 solution

--- d1a1-master/account-management/src/main/java/ph/apper/account/App.java
+++ d1a1-master/account-management/src/main/java/ph/apper/account/App.java
@@ -1,11 +1,36 @@
 package ph.apper.account;
 
+import lombok.Data;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
+    }
+
+    @RestController
+    @RequestMapping("account")
+    public static class AccountController{
+        @PostMapping
+        public ResponseEntity create(@RequestBody Request request){
+            System.out.println(request);
+
+            return ResponseEntity.ok().build();
+        }
+    }
+
+    @Data
+    public static class Request{
+        private String firstName;
+        private String lastName;
+        private String email;
+        private String password;
     }
 }
